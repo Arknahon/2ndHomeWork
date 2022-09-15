30a6d8a40c1a13ebd60fd7ddf9daa73f3d804510
Arknahon <bakirov090303@gmail.com>
2022-09-15 18:18:08 +0600

    new function

--- 2ndHomeWork/main.swift
+++ 2ndHomeWork/main.swift
@@ -1,11 +1,49 @@
-//
-//  main.swift
-//  2ndHomeWork
-//
-//  Created by user on 13/9/22.
-//
+//Second Home Work №1
 
-import Foundation
+//Нахождение кв числа
+func getSquare(_ num: Int) -> Int {
+    return num * num
+}
+func getSquareString(string: String) -> String {
+    return string + string
+}
+print(getSquare(5))
 
-print("Hello, World!")
 
+//Нахождение куба числа
+func cube(numTwo: Int) {
+    print("\(numTwo) * \(numTwo) * \(numTwo) = \(numTwo * numTwo * numTwo)")
+}
+let numTwo = readLine()!
+cube(numTwo: Int(numTwo) ?? 0)
+
+
+//Нахождение периметра куба
+func perimeter(numThree: Int){
+    print("12 * \(numThree) = \(12 * numThree)")
+}
+let numThree = readLine()!
+perimeter(numThree: Int(numThree) ?? 0)
+
+
+//Нахождение площади прямоугольника
+func ploshyad(numFour: Int , numFive: Int) {
+    print("\(numFour) * \(numFive) = \(numFour * numFive)")
+}
+let numFour = readLine()!
+let numFive = readLine()!
+ploshyad(numFour: Int(numFour) ?? 0, numFive: Int(numFive) ?? 0)
+
+
+//Площадь круга
+func getKrug(_ numSix: Double) -> Double {
+    return numSix * 3.14
+}
+func getKrugString(_ string: String) -> String {
+    return string + string
+}
+print(getKrug(5))
+
+
+
+//Second Home Work №2
